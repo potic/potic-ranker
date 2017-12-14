70848962fd1dd409c5f076613c0ec9b1bf390912
Yaroslav Yermilov <yermilov17@gmail.com>
2017-12-15 01:35:04 +0200

    #2. improvements for store ranks

--- potic-ranker.py
+++ potic-ranker.py
@@ -1,16 +1,25 @@
 from flask import Flask, request, Response
 import json
 import random
+import os
+
+ENVIRONMENT_NAME = "dev"
+if 'ENVIRONMENT_NAME' in os.environ:
+    ENVIRONMENT_NAME = os.environ['ENVIRONMENT_NAME']
 
 app = Flask(__name__)
 
 
-@app.route('/rank', methods=['POST'])
-def rank():
-    articles = request.json
-    ranks = map(lambda x: random.random(), articles)
-    return Response(response=json.dumps(ranks), status=200, mimetype="application/json")
+@app.route('/actual')
+def actulRank():
+    return Response(response=json.dumps("random"), status=200, mimetype="application/json")
+
+@app.route('/rank/<rank_id>', methods=['POST'])
+def rank(rank_id):
+    article = request.json
+    rank = random.random()
+    return Response(response=json.dumps(rank), status=200, mimetype="application/json")
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=(40409 if ENVIRONMENT_NAME == "dev" else 5000))
