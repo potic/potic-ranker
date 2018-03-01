13df1bb74ac1690c8a4915b54a5003480b940007
Yaroslav Yermilov <yermilov17@gmail.com>
2018-03-02 01:38:32 +0200

    various improvements

--- potic-ranker.py
+++ potic-ranker.py
@@ -14,48 +14,72 @@ ENVIRONMENT_NAME = "dev"
 if 'ENVIRONMENT_NAME' in os.environ:
     ENVIRONMENT_NAME = os.environ['ENVIRONMENT_NAME']
 
+LOGZIO_TOKEN = None
 if 'LOGZIO_TOKEN' in os.environ:
     LOGZIO_TOKEN = os.environ['LOGZIO_TOKEN']
 if os.path.isfile('logzio-dev.properties'):
     with open('logzio-dev.properties', 'r') as logzioTokenFile:
         LOGZIO_TOKEN = logzioTokenFile.read().replace('\n', '')
-LOGGING_CONFIG = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': '%(asctime)s [%(thread)d] %(levelname)s %(module)s - %(message)s'
+if LOGZIO_TOKEN != None:
+    LOGGING_CONFIG = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'verbose': {
+                'format': '%(asctime)s [%(thread)d] %(levelname)s %(module)s - %(message)s'
+            },
+            'logzioFormat': {
+                'format': '{"env": "' + ENVIRONMENT_NAME + '", "service": "potic-ranker"}'
+            }
         },
-        'logzioFormat': {
-            'format': '{"env": "' + ENVIRONMENT_NAME + '", "service": "potic-ranker"}'
-        }
-    },
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-            'level': 'INFO' if ENVIRONMENT_NAME == "prod" else 'DEBUG',
-            'formatter': 'verbose'
+        'handlers': {
+            'console': {
+                'class': 'logging.StreamHandler',
+                'level': 'INFO' if ENVIRONMENT_NAME == "prod" else 'DEBUG',
+                'formatter': 'verbose'
+            },
+            'logzio': {
+                'class': 'logzio.handler.LogzioHandler',
+                'level': 'INFO' if ENVIRONMENT_NAME == "prod" else 'DEBUG',
+                'formatter': 'logzioFormat',
+                'token': LOGZIO_TOKEN,
+                'logs_drain_timeout': 5,
+                'url': 'https://listener.logz.io:8071',
+                'debug': True
+            }
         },
-        'logzio': {
-            'class': 'logzio.handler.LogzioHandler',
-            'level': 'INFO' if ENVIRONMENT_NAME == "prod" else 'DEBUG',
-            'formatter': 'logzioFormat',
-            'token': LOGZIO_TOKEN,
-            'logs_drain_timeout': 5,
-            'url': 'https://listener.logz.io:8071',
-            'debug': True
-        },
-    },
-    'loggers': {
-        'potic-ranker': {
-            'handlers': ['console', 'logzio'],
-            'level': 'INFO' if ENVIRONMENT_NAME == "prod" else 'DEBUG',
+        'loggers': {
+            'potic-ranker': {
+                'handlers': ['console', 'logzio'],
+                'level': 'INFO' if ENVIRONMENT_NAME == "prod" else 'DEBUG',
+            }
         }
     }
-}
-logging.config.dictConfig(LOGGING_CONFIG)
+else:
+    LOGGING_CONFIG = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'verbose': {
+                'format': '%(asctime)s [%(thread)d] %(levelname)s %(module)s - %(message)s'
+            }
+        },
+        'handlers': {
+            'console': {
+                'class': 'logging.StreamHandler',
+                'level': 'INFO' if ENVIRONMENT_NAME == "prod" else 'DEBUG',
+                'formatter': 'verbose'
+            }
+        },
+        'loggers': {
+            'potic-ranker': {
+                'handlers': ['console'],
+                'level': 'INFO' if ENVIRONMENT_NAME == "prod" else 'DEBUG',
+            }
+        }
+    }
 
-app = Flask(__name__)
+logging.config.dictConfig(LOGGING_CONFIG)
 
 
 class CustomBinarizer(BaseEstimator, TransformerMixin):
@@ -72,12 +96,16 @@ if os.path.isfile('serialized_model'):
     with open('serialized_model', 'r') as serialized_model_file:
         model = pickle.loads(serialized_model_file.read())
 
+model_id = "logreg:0.1"
+
+app = Flask(__name__)
+
 
 @app.route('/actual')
 def actulRank():
     try:
         logging.getLogger('potic-ranker').debug("receive GET request for /actual", extra={'loglevel':'DEBUG'})
-        return Response(response=json.dumps("logreg"), status=200, mimetype="application/json")
+        return Response(response=json.dumps(model_id), status=200, mimetype="application/json")
     except:
         logging.getLogger('potic-ranker').error("GET request for /actual failed", extra={'loglevel':'ERROR'})
         return Response(status=500)
@@ -85,18 +113,20 @@ def actulRank():
 @app.route('/rank/<rank_id>', methods=['POST'])
 def rank(rank_id):
     try:
-        logging.getLogger('potic-ranker').debug("receive POST request for /rank/" + str(rank_id) + "; body=" + str(request.json), extra={'loglevel':'DEBUG'})
-        article = request.json
-        source = article["card"]["source"] if "card" in article else ""
-        word_count = int(article["fromPocket"]["word_count"]) if "fromPocket" in article else 0
-        model_input = np.array([(word_count, source)], dtype=[('word_count', 'int'), ('source', 'object')])
-        rank = model.predict_proba(model_input)[0][1]
-        logging.getLogger('potic-ranker').debug("received word_count " + str(word_count) + ", source " + str(source) + ", calculated rank " + str(rank),
-                                                extra={'loglevel': 'DEBUG'})
-        #rank = random.random()
-        return Response(response=json.dumps(rank), status=200, mimetype="application/json")
+        if rank_id == model_id:
+            logging.getLogger('potic-ranker').debug("receive POST request for /rank/" + str(rank_id) + "; body=" + str(request.json), extra={'loglevel':'DEBUG'})
+            article = request.json
+            source = article["card"]["source"] if "card" in article else ""
+            word_count = int(article["fromPocket"]["word_count"]) if "fromPocket" in article else 0
+            model_input = np.array([(word_count, source)], dtype=[('word_count', 'int'), ('source', 'object')])
+            rank = model.predict_proba(model_input)[0][1]
+            logging.getLogger('potic-ranker').debug("received word_count " + str(word_count) + ", source " + str(source) + ", calculated rank " + str(rank), extra={'loglevel': 'DEBUG'})
+            return Response(response=json.dumps(rank), status=200, mimetype="application/json")
+        else:
+            logging.getLogger('potic-ranker').error("Unknown model " + str(rank_id), extra={'loglevel':'ERROR'})
+            return Response(status=404)
     except Exception as e:
-        logging.getLogger('potic-ranker').error("POST request for /rank/" + rank_id + " failed: " + str(e), extra={'loglevel':'ERROR'})
+        logging.getLogger('potic-ranker').error("POST request for /rank/" + str(rank_id) + " failed: " + str(e), extra={'loglevel':'ERROR'})
         return Response(status=500)
 
 
